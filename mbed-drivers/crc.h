c27b426b71106618933bf315d5c488eaf1aa5122
Daniel Cesarini <daniel.cesarini@tridonic.com>
2016-07-05 17:05:54 +0200

    Implemented layer for CRC32 calculation
    - Implemented the _Calculate function (that uses the accumulate function inside, plus a preliminary reset of the CRC module):
    -- The _Calculate function is padding the data to be multiple of 4 (with 0s).
    - As a backup solution a software implementation of the CRC calculation can be enabled with proper setting of defines.

--- mbed-drivers/crc.h
+++ mbed-drivers/crc.h
@@ -1,13 +1,22 @@
-/***************************************************
-*  crc.h
-*
-*  
-*
-*  Tridonic GmbH & Co KG
-*  Färbergasse 15
-*  6851 Dornbirn, Austria
-*
-***************************************************/
+/*************************************************************************//**
+ *
+ * \file    crc.h
+ * \brief   ...
+ *
+ * \note    None
+ *
+ * \author  Daniel Cesarini <daniel.cesarini@tridonic.com>
+ * \author  Martin Tomasini <martin.tomasini2@tridonic.com>
+ *
+ * \version 0.1
+ * \date    4 July 2016
+ * \bug     None
+ *
+ * \copyright 2016, Tridonic GmbH & Co KG
+ *                  Färbergasse 15 - 6851 Dornbirn, Austria
+ *
+ *****************************************************************************/
+
 
 #ifndef YOTTA_MODULES_MBED_DRIVERS_MBED_DRIVERS_CRC_H_
 #define YOTTA_MODULES_MBED_DRIVERS_MBED_DRIVERS_CRC_H_
@@ -18,33 +27,31 @@
 
 /* Public typedef -----------------------------------------------------------*/
 /* Public define ------------------------------------------------------------*/
+#define HW_CRC
+#define CRC_INITVALUE 0xFFFFFFFF
+
 /* Public macro -------------------------------------------------------------*/
 /* Exported variables -------------------------------------------------------*/
 /* Public function prototypes -----------------------------------------------*/
 namespace mbed
 {
-    class crc
+    class Crc
     {
-
     public:
-
-        typedef struct
-        {
-            uint32_t ui32NumOfSectors;
-            uint32_t ui32StartAddress;
-            uint32_t ui32EndAddress;
-            uint32_t ui32SectorSize;
-        }SectorInfo_td;
-
-        crc();
-        ~crc();
+        Crc();
+        ~Crc();
+        int _UseCrc(void);
+        int _ReleaseCrc(void);
         void _Reset(void);
-        int _CalcSingle(uint8_t* pui32StartAddress, uint32_t ui32DataSize, uint32_t* pui32CRC);
-        int _CalcAccumulate(uint8_t* pui32StartAddress, uint32_t ui32DataSize, uint32_t* pui32CRC);
+        int _Calculate(uint8_t* pui8StartAddress, uint32_t ui32DataSize, uint32_t* pui32CRC);
 
     private:
+        int _Accumulate(uint8_t* pui8StartAddress, uint32_t ui32DataSize, uint32_t* pui32CRC);
+        void acquireMutex();
+        void releaseMutex();
         static uint32_t InstanceCounter;
-
+        static bool Initialized;
+        uint32_t localCrc;
     };
 } // namespace mbed
 
